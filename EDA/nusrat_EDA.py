d0c748c31ede6f479887d3c403d8fd61825ed939
nprithee <nprithee@gwu.edu>
2022-12-02 14:48:26 -0500

    commit

--- EDA/nusrat_EDA.py
+++ EDA/nusrat_EDA.py
@@ -26,5 +26,22 @@ df.describe()
 print('finding mising value')
 print(df.isnull().sum())
 
+# %%
+creditscore_count = df['CreditScore'].value_counts()
+print(creditscore_count)
+
+sns.boxplot(data=df, x="CreditScore")
+
+# %%
+print('Histogram of CreditScore  by CreditCard')
+df.hist(column='CreditScore', by='HasCrCard', grid=True, xlabelsize=None, xrot=None,
+            ylabelsize=None, yrot=None, ax=None, sharex=False, sharey=False,
+            figsize=None, layout=None, bins=30, backend=None, legend=True)
+# %%
+
+
+# %%
+
+
 
 # %%
