16aab5885e6bf2d9daa6de939e614eb1231f2373
jtian10 <atian2319@gmail.com>
2022-12-02 12:00:50 -0500

    eda with pie chart

--- EDA/Steven_EDA.py
+++ EDA/Steven_EDA.py
@@ -4,6 +4,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns 
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.formula.api import glm
+from statsmodels.formula.api import ols
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 # %%
 #Load Data
 df=pd.read_csv(r"/Users/stephantian/Desktop/Grad School/Course/6103 Data Mining/T1-Data_Ninjas-22FA/Data/Bank_data.csv")
@@ -14,6 +19,7 @@ geo = df['Geography']
 churnF = df[df['Geography'] == 'France']
 churnG = df[df['Geography'] == 'Germany']
 churnS = df[df['Geography'] == 'Spain']
+df['Geography'].describe()
 
 x=df['Geography'].value_counts()
 x=x.sort_index()
@@ -21,4 +27,5 @@ x=x.sort_index()
 churn = df['Geography']
 churn.value_counts().plot(kind='pie')
 plt.xlabel('# of Churn by country', fontsize=12)
+plt.show()
 # %%
