708a68bcb2d22a39ade5277188df1d65332340c7
Vishesh Bhati <vishesh.bhati88@gmail.com>
2022-12-09 15:13:37 -0500

    Update Vishesh_Modeling.py
    fetched the data from the file and created a new test-train split.

--- Modeling/Vishesh_Modeling.py
+++ Modeling/Vishesh_Modeling.py
@@ -4,28 +4,45 @@
 import pandas as pd
 import numpy as np
 import pickle as pk
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+# %%
+# Imorting dataset
+Bank_data = pd.read_csv(r"C:\Users\vishesh\Documents\GitHub\T1-Data_Ninjas-22FA\Data\Bank_data.csv")
+
+print(f"Bank_data.shape = {Bank_data.shape}")
+print(f"Bank_data.info() = {Bank_data.info()}")
+print(f"Bank_data.head() = {Bank_data.head()}")
 
 
 # %%
 ## Loading preprocessed data using pickle
 
-x_train=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\x_train.pk", "rb" ) )
-y_train=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\y_train.pk", "rb" ) )
+#x_train=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\x_train.pk", "rb" ) )
+#y_train=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\y_train.pk", "rb" ) )
 
-x_test=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\x_test.pk", "rb" ) )
-y_test=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\y_test.pk", "rb" ) )
-print("Train Data Shape")
-print(x_train.shape)
-print(y_train.shape)
+#x_test=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\x_test.pk", "rb" ) )
+#y_test=pk.load( open( r"C:\Users\vishesh\Documents\GWU\Data Mining\Project\Program\y_test.pk", "rb" ) )
+#print("Train Data Shape")
+#print(x_train.shape)
+#print(y_train.shape)
 
-print("Test Data Shape")
-print(x_test.shape)
-print(y_test.shape)
+#print("Test Data Shape")
+#print(x_test.shape)
+#print(y_test.shape)
+
+# %%
+# Creating Test Train split
+x=Bank_data[["CustomerId", "Surname", "CreditScore", "Geography", "Gender", "Age", "Tenure", "Balance","NumOfProducts","HasCrCard", "IsActiveMember", "EstimatedSalary"]]
+y=Bank_data["Exited"]
+							
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=321)
 
 # %%
 
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import classification_report
 clf = GaussianNB()
 clf.fit(x_train, y_train)
 
@@ -36,3 +53,5 @@ print(clf.score(x_test,y_test))
 
 y_pred=clf.predict(x_test)
 print(classification_report(y_test,y_pred))
+
+# %%
