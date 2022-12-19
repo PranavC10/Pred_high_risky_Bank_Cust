89569b923bf44c6121da40b785e6bc9fccc4dfdc
jtian10 <atian2319@gmail.com>
2022-12-19 17:07:01 -0500

    Update Code_technical_Analysis.py

--- Code_technical_Analysis.py
+++ Code_technical_Analysis.py
@@ -251,6 +251,64 @@ rand_clf_tune = RandomForestClassifier(criterion= 'entropy',
  min_samples_split= 4,
  n_estimators = 115,random_state=6)
 
-rand_clf_tune.fit(x_train,y_train)
-evaluate_model(rand_clf_tune,x_train,y_train,X_test,y_test,fit=True)
+rand_clf_tune.fit(X_train,y_train)
+evaluate_model(rand_clf_tune,X_train,y_train,X_test,y_test,fit=True)
+
 # %%
+#KNN Model
+print("Train Data Shape")
+print(x_train.shape)
+print(y_train.shape)
+
+print("Test Data Shape")
+print(X_test.shape)
+print(y_test.shape)
+
+#Import KNN package
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1) # instantiate with n value given
+knn.fit(X_train,y_train)
+y_pred = knn.predict(X_train)
+y_pred = knn.predict_proba(X_train)
+print(y_pred)
+print(knn.score(X_train,y_train))
+print('\n','Next Line')
+from sklearn.neighbors import KNeighborsClassifier
+knn_split = KNeighborsClassifier(n_neighbors=1)
+knn_split.fit(X_train,y_train)
+ytest_pred = knn_split.predict(X_test)
+ytest_pred
+print(knn_split.score(X_test,y_test))
+
+from sklearn.model_selection import GridSearchCV
+para = {'n_neighbors':[1,2,3,5,6,7,9,10]}
+knn_cv = KNeighborsClassifier()
+from sklearn.model_selection import cross_val_score
+cv_results = cross_val_score(knn_cv,X_train, y_train, cv=7)
+n = GridSearchCV(knn_cv,para,cv=7)
+n.fit(X_train,y_train)
+print({(n.best_params_)['n_neighbors']})
+print(n.best_score_)
+print(cv_results) 
+print(np.mean(cv_results)) 
+# %%
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+ 
+cm = confusion_matrix(y_test,ytest_pred)
+print(cm)
+ 
+color = 'white'
+matrix = plot_confusion_matrix(knn, X_test, y_test, cmap=plt.cm.Blues)
+matrix.ax_.set_title('Confusion Matrix', color=color)
+plt.xlabel('Predicted Label', color=color)
+plt.ylabel('True Label', color=color)
+plt.gcf().axes[0].tick_params(colors=color)
+plt.gcf().axes[1].tick_params(colors=color)
+plt.show()
+
+# %%
+from sklearn.metrics import classification_report
+ 
+print(classification_report(y_test, ytest_pred))
+
